3962cede109b454b81330a4b23d1a198f57a4556
zeinab <ms.zeinab.ahmadi@gmail.com>
2026-07-18 12:58:49 -0700

    feat: create personal information page layout

---
+++ src/components/PersonalInfoPage.jsx
@@ -0,0 +1,27 @@
+import InfoPage from "./InfoPage";
+import ProgressBar from "./ProgressBar";
+
+const PersonalInfoPage = () => {
+    
+    return(
+        <div className="min-h-screen flex items-center justify-center pt-20 p-8">
+            
+            <div className="w-full max-w-7xl rounded-3xl backdrop-blur-xl bg-[#071411]/40 border border-emerald-900/40 p-10 shadow-2xl shadow-emerald-950/40 flex gap-10">
+                <div className="flex flex-col flex-[3]">
+                    <ProgressBar progress={17} />
+                    
+                    <InfoPage />
+                </div>
+
+                <div className="w-[1px] self-stretch bg-emerald-900/40"></div>
+
+                <div className="text-white flex-[1]">
+                    lefttttttt
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default PersonalInfoPage;
